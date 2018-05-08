c4c692c223aba61d433a367883db50669df8e980
Bernhard Landsberg <bernhardlandsberg@BEL-MacBook-Pro.local>
2018-05-08 13:55:34 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="q-app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+console.log('test1')
+
+export default {
+  name: 'App'
+}
+
+methods: {
+  
+}
+</script>
+
+<style>
+</style>
